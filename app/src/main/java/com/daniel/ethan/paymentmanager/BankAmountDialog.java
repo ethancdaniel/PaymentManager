ce02c475f50c2784c722c204985de0895edb56b6
Ethan Daniel <ethandaniel@berkeley.edu>
2019-07-27 21:54:33 -0700

    Use AppCompatDialogFragment instead of DialogFragment

--- app/src/main/java/com/daniel/ethan/paymentmanager/BankAmountDialog.java
+++ app/src/main/java/com/daniel/ethan/paymentmanager/BankAmountDialog.java
@@ -4,7 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 import static com.daniel.ethan.paymentmanager.Utils.isFilled;
 
-public class BankAmountDialog extends DialogFragment {
+public class BankAmountDialog extends AppCompatDialogFragment {
     EditText bankAmount;
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
--- app/src/main/java/com/daniel/ethan/paymentmanager/NewEnvelopeDialog.java
+++ app/src/main/java/com/daniel/ethan/paymentmanager/NewEnvelopeDialog.java
@@ -4,7 +4,7 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
@@ -12,7 +12,7 @@ import android.widget.Toast;
 
 import static com.daniel.ethan.paymentmanager.Utils.isFilled;
 
-public class NewEnvelopeDialog extends DialogFragment {
+public class NewEnvelopeDialog extends AppCompatDialogFragment {
     EditText envelopeName;
     EditText envelopeAmount;
     EditText envelopeAutoUpdate;
