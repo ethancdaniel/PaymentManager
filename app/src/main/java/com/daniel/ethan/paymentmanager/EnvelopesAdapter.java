4e7fc23559d7ff58f57fa599e27179735e3cdbd6
Ethan Daniel <ethandaniel@berkeley.edu>
2019-08-04 13:39:00 -0700

    Support swiping gestures on envelopes

--- app/src/main/java/com/daniel/ethan/paymentmanager/EnvelopesAdapter.java
+++ app/src/main/java/com/daniel/ethan/paymentmanager/EnvelopesAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,11 +13,17 @@ import androidx.cardview.widget.CardView;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.swipe.SimpleSwipeListener;
+import com.daimajia.swipe.SwipeLayout;
+import com.daimajia.androidanimations.library.YoYo;
+import com.daimajia.swipe.adapters.RecyclerSwipeAdapter;
+
 import java.util.ArrayList;
 
 import static com.daniel.ethan.paymentmanager.Utils.formatMoney;
 
-public class EnvelopesAdapter extends RecyclerView.Adapter<EnvelopesAdapter.ViewHolder> {
+public class EnvelopesAdapter extends RecyclerSwipeAdapter<EnvelopesAdapter.ViewHolder> {
 
     private ArrayList<String> envelopeNames;
     private ArrayList<Double> envelopeCurrentAmounts;
@@ -34,12 +41,6 @@ public class EnvelopesAdapter extends RecyclerView.Adapter<EnvelopesAdapter.View
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_envelope, viewGroup, false);
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
@@ -49,12 +50,8 @@ public class EnvelopesAdapter extends RecyclerView.Adapter<EnvelopesAdapter.View
         viewHolder.envelopeName.setText(envelopeNames.get(i));
         viewHolder.currentAmount.setText(formatMoney(envelopeCurrentAmounts.get(i)));
         viewHolder.autoUpdateAmount.setText(formatMoney(envelopeAutoUpdateAmounts.get(i)));
-        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(mContext, envelopeNames.get(i) + " clicked.", Toast.LENGTH_SHORT).show();
-            }
-        });
+
+        viewHolder.swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
     }
 
     @Override
@@ -62,17 +59,26 @@ public class EnvelopesAdapter extends RecyclerView.Adapter<EnvelopesAdapter.View
         return envelopeNames.size();
     }
 
+    @Override
+    public int getSwipeLayoutResourceId(int position) {
+        return R.id.swipe;
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView envelopeName;
         TextView currentAmount;
         TextView autoUpdateAmount;
-        CardView cardView;
+        SwipeLayout swipeLayout;
+        ImageView editImageView;
+        ImageView deleteImageView;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             envelopeName = itemView.findViewById(R.id.text_envelope_name);
             currentAmount = itemView.findViewById(R.id.text_current_amount);
             autoUpdateAmount = itemView.findViewById(R.id.text_auto_update_amount);
-            cardView = itemView.findViewById(R.id.card_envelope);
+            swipeLayout = itemView.findViewById(R.id.swipe);
+            editImageView = itemView.findViewById(R.id.edit_envelope);
+            deleteImageView = itemView.findViewById(R.id.delete_envelope);
         }
     }
 }
